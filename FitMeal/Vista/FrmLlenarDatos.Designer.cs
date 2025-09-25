24f5da847814860eec083eb6798b6fca11da731b
JuanJo0512 <florezcardonaj@gmail.com>
2025-09-25 16:51:19 -0500

    Agregado archivo .gitignore para ignorar archivos innecesarios

---
+++ FitMeal/Vista/FrmLlenarDatos.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace FitMeal.Vista
+{
+    partial class FrmLlenarDatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnCrearPerfil = new System.Windows.Forms.Button();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.cmbSexo = new System.Windows.Forms.ComboBox();
+            this.cmbMeta = new System.Windows.Forms.ComboBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label1.Location = new System.Drawing.Point(37, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bienvenido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label2.Location = new System.Drawing.Point(41, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cedula:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label3.Location = new System.Drawing.Point(42, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(328, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Por favor, completa los siguientes datos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label4.Location = new System.Drawing.Point(41, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nombre(s):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label5.Location = new System.Drawing.Point(41, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 30);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Apellido(s):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label6.Location = new System.Drawing.Point(41, 502);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(136, 30);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Altura(m):";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label7.Location = new System.Drawing.Point(41, 445);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 30);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Sexo:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label8.Location = new System.Drawing.Point(41, 387);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 30);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Edad:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label9.Location = new System.Drawing.Point(41, 335);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 30);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Email:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label10.Location = new System.Drawing.Point(41, 614);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 30);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Meta:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label11.Location = new System.Drawing.Point(41, 556);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(136, 30);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "Peso(Kg):";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label12.Location = new System.Drawing.Point(41, 671);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(143, 30);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Contraseña:";
+            // 
+            // btnCrearPerfil
+            // 
+            this.btnCrearPerfil.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnCrearPerfil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnCrearPerfil.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnCrearPerfil.Location = new System.Drawing.Point(46, 753);
+            this.btnCrearPerfil.Name = "btnCrearPerfil";
+            this.btnCrearPerfil.Size = new System.Drawing.Size(158, 44);
+            this.btnCrearPerfil.TabIndex = 13;
+            this.btnCrearPerfil.Text = "Crear Perfil";
+            this.btnCrearPerfil.UseVisualStyleBackColor = false;
+            this.btnCrearPerfil.Click += new System.EventHandler(this.btnCrearPerfil_Click);
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(143, 177);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(219, 22);
+            this.txtCedula.TabIndex = 14;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(196, 237);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(219, 22);
+            this.txtNombre.TabIndex = 15;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(198, 292);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(219, 22);
+            this.txtApellido.TabIndex = 16;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(129, 345);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(219, 22);
+            this.txtEmail.TabIndex = 17;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(123, 397);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(219, 22);
+            this.txtEdad.TabIndex = 18;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(183, 564);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(219, 22);
+            this.txtPeso.TabIndex = 19;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(183, 512);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(219, 22);
+            this.txtAltura.TabIndex = 20;
+            // 
+            // cmbSexo
+            // 
+            this.cmbSexo.FormattingEnabled = true;
+            this.cmbSexo.Items.AddRange(new object[] {
+            "Masculino",
+            "Femenino"});
+            this.cmbSexo.Location = new System.Drawing.Point(135, 455);
+            this.cmbSexo.Name = "cmbSexo";
+            this.cmbSexo.Size = new System.Drawing.Size(176, 24);
+            this.cmbSexo.TabIndex = 21;
+            // 
+            // cmbMeta
+            // 
+            this.cmbMeta.FormattingEnabled = true;
+            this.cmbMeta.Items.AddRange(new object[] {
+            "Perder Peso",
+            "Ganar Masa"});
+            this.cmbMeta.Location = new System.Drawing.Point(121, 624);
+            this.cmbMeta.Name = "cmbMeta";
+            this.cmbMeta.Size = new System.Drawing.Size(176, 24);
+            this.cmbMeta.TabIndex = 22;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(190, 681);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(219, 22);
+            this.txtContraseña.TabIndex = 23;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnVolver.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnVolver.Location = new System.Drawing.Point(552, 753);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(158, 44);
+            this.btnVolver.TabIndex = 24;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // FrmLlenarDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(800, 824);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.cmbMeta);
+            this.Controls.Add(this.cmbSexo);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.btnCrearPerfil);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmLlenarDatos";
+            this.Text = "FrmLlenarDatos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnCrearPerfil;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.ComboBox cmbMeta;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnVolver;
+    }
+}
---
+++ FitMeal/Vista/FrmLoggin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FitMeal.Vista
+{
+    public partial class FrmLoggin : Form
+    {
+        public FrmLoggin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrearCuenta_Click(object sender, EventArgs e)
+        {
+            AbrirForm(new FrmLlenarDatos());
+        }
+
+        Form activeForm = null;
+
+        private void AbrirForm(Form frmHijo)
+        {
+            if (activeForm != null && !activeForm.IsDisposed)
+            {
+                activeForm.Close();
+            }
+
+            activeForm = frmHijo;
+            frmHijo.StartPosition = FormStartPosition.CenterScreen;
+            frmHijo.Show();
+            this.Hide();
+        }
+
+    }
+}
