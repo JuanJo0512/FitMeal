85fc03aba4db1a026820ac825ddcac5e385c76e7
SrtaCano <canomenesesvalentina@gmail.com>
2025-10-04 12:04:25 -0500

    cambios en frmMiPerfil

--- FitMeal/Vista/FrmPerfil.cs
+++ FitMeal/Vista/FrmPerfil.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using FitMeal.Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,50 @@ namespace FitMeal.Vista
 {
     public partial class FrmPerfil : Form
     {
+
+        // Creando un Objeto de cada una de las clases
+        // cn para conexion
+        // cmd para los comandos
+        // da para la adaptacion de datos
+        // dt para sacar las tablas de la database
+        cConexion cn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
+        int i, contador, boton;
         public FrmPerfil()
         {
             InitializeComponent();
+
+            cn = new cConexion();
+
+            //Se inicializa el subindice y el boton en cero
+            i = 0;
+            boton = 0;
+
+            // Aqui comenzamos a usar el lenguaje de sql, por eso usamos el SqlCommand y como vamos a usar la base de datos llamamos el metodo de abrir conexion
+            cmd = new SqlCommand("Select * from tblEmpleado", cn.AbrirConexion());
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+
+            // llenar tiene 2 parametros, el dt que es toda la tabla y la i que es el subindice
+            llenar(dt, i);
+        }
+
+        private void llenar(DataTable dt, int i)
+        {
+            
+            txtEdad.Text = dt.Rows[i][4].ToString();
+            txtSexo.Text = dt.Rows[i][5].ToString();
+            txtAltura.Text = dt.Rows[i][6].ToString();
+            txtPeso.Text = dt.Rows[i][7].ToString();
+            // Aqui iria el meta que es un combo box pero no se como ponerlo
+            txtEmail.Text = dt.Rows[i][9].ToString();
+            txtContraseña.Text = dt.Rows[i][10].ToString();
+
+            contador = dt.Rows.Count;
+
         }
 
         private void FrmPerfil_Load(object sender, EventArgs e)
